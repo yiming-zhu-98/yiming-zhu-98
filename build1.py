d184c614415459ab27078efac493736db90b3c10
yiming-zhu-98 <13578873661@163.com>
2026-03-23 09:50:23 +0800

    support CDN image cache

--- build1.py
+++ build1.py
@@ -29,7 +29,7 @@ from datetime import datetime
 
 GITHUB_USER = "yiming-zhu-98"          # your GitHub username
 GITHUB_REPO = "yiming-zhu-98.github.io"   # your repository name
-GITHUB_BRANCH = "version1"                  # branch (usually main or master)
+GITHUB_BRANCH = "zym"                  # branch (usually main or master)
 
 # Set to True to use jsDelivr CDN for all images/
 USE_CDN = True
